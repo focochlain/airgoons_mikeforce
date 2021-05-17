cf5f5555848e8a545112f3bcf9860445d73afa61
wonkotron <wonkotron@gmail.com>
2021-05-16 18:23:13 -0700

    Mission pack unpack (#5)
    * Basic config file
    
    * Update gitignore for VisualStudio projects
    
    * Initial commit for mission packaging utility
    
    * Implement basic config file, rough-in functions
    
    * Add SteamCmdPath to config settings
    
    * Initial implementation

---
+++ mission_pack_unpack/Configuration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Config.Net;
+
+namespace mission_pack_unpack {
+    public static class Configuration {
+        public interface ISettings {
+            string Arma3Tools_Path { get; }
+            string MikeForce_Path { get; }
+            string Customizations_Path { get; }
+            string Output_Path { get; }
+        }
+
+        public static ISettings Settings {
+            get {
+                if (_settings == null) {
+                    // Settings accessed before init, attempt to init
+                    InitSettings();
+                }
+
+                return _settings;
+            }
+        }
+        private static ISettings _settings = null;
+
+        public static void InitSettings() {
+            string filepath = "configuration.json";
+
+            if (!System.IO.File.Exists(filepath)) {
+                throw new ApplicationException(
+                    string.Format("Configuration file does not exist:  {0}", filepath)
+                );
+            }
+
+            else {
+                Console.Out.WriteLine("Loading settings from {0}", filepath);
+
+                _settings = new ConfigurationBuilder<ISettings>()
+                    .UseJsonFile(filepath)
+                    .Build();
+            }
+
+            if (_settings == null) {
+                throw new ApplicationException("null settings after attempt to initialize");
+            }
+        }
+    }
+}
---
+++ mission_pack_unpack/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using Config.Net;
+
+namespace mission_pack_unpack {
+
+    class Program {
+        static int Main() {
+
+            /*
+             * Mission Pack Unpack
+             * - Initialize settings from configuration file
+             * - Copy Mike Force PBO from Steam Workshop
+             * - Unpack PBO using Arma 3 Tools
+             * - Copy AirGoons customizations
+             * - Package PBO for manual deployment to server
+             * - Clean up
+             */
+
+            try {
+                string workingDir = Path.GetFullPath("./temp");
+
+                CleanUp(workingDir);
+
+                Configuration.InitSettings();
+                string mikeForcePBO_path = CopyMikeForcePBO(workingDir);
+                string customMission_path = UnpackPBO(mikeForcePBO_path);
+                CustomizeMission(customMission_path);
+                PackPBO(customMission_path);
+
+                CleanUp(workingDir);
+
+                return 0;
+            }
+            catch (Exception ex) {
+                Console.Error.WriteLine(ex.ToString());
+                return 1;
+            }
+        }
+
+        static string CopyMikeForcePBO(string workingDir) {
+            Console.Out.WriteLine("Downloading official Mike Force PBO");
+
+            string filename = "1799728943668364634_legacy.bin";
+            string mikeforce_filepath = string.Format(
+                "{0}/{1}",
+                Configuration.Settings.MikeForce_Path,
+                filename);
+
+            if (!File.Exists(mikeforce_filepath)) {
+                throw new ApplicationException(string.Format("Mike Force workshop file does not exist:  {0}", mikeforce_filepath));
+            }
+
+            else {
+                
+                string destinationPath = string.Format("{0}/{1}", workingDir, "mikeforce_latest.pbo");
+                Directory.CreateDirectory(workingDir);
+                File.Copy(mikeforce_filepath, destinationPath, true);
+
+                return destinationPath;
+            }
+        }
+
+        static string UnpackPBO(string pboPath) {
+            Console.Out.WriteLine("Unpacking official Mike Force PBO");
+
+            if (!File.Exists(pboPath)) {
+                throw new ApplicationException(string.Format("PBO file does not exist: {0}", pboPath));
+            }
+            else {
+                string bankRev_path = string.Format(
+                    "{0}/BankRev/BankRev.exe",
+                    Configuration.Settings.Arma3Tools_Path
+                );
+
+                var p = new System.Diagnostics.Process();
+                p.StartInfo.FileName = bankRev_path;
+                p.StartInfo.Arguments = pboPath;
+                p.Start();
+                p.WaitForExit();
+
+                File.Move(pboPath, pboPath.Replace("mikeforce_latest.pbo", "mikeforce_latest_official.pbo"));
+
+                string oldPath = pboPath.Replace(".pbo", "");
+                string newPath = pboPath.Replace(".pbo", "_airgoons");
+                Directory.Move(oldPath, newPath);
+
+                return newPath;
+            }
+        }
+
+        static void CustomizeMission(string customMission_path) {
+            Console.Out.WriteLine("Applying customizations");
+            var files = Directory.GetFiles(Configuration.Settings.Customizations_Path, "*.*", SearchOption.AllDirectories);
+            foreach (var file in files) {
+                File.Copy(file, file.Replace(Configuration.Settings.Customizations_Path, customMission_path), true);
+            }
+        }
+
+        static void PackPBO(string customMission_path) {
+            Console.Out.WriteLine("Packing AirGoons Mike Force PBO");
+
+            string fileBank_path = string.Format(
+                "{0}/FileBank/FileBank.exe",
+                Configuration.Settings.Arma3Tools_Path
+            );
+
+            var p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = fileBank_path;
+            p.StartInfo.Arguments = customMission_path;
+            p.Start();
+            p.WaitForExit();
+
+            File.Move(string.Format("{0}.pbo", customMission_path), Configuration.Settings.Output_Path);
+        }
+
+        static void CleanUp(string workingDir) {
+            Console.Out.WriteLine("Clean up working directories");
+
+            if (Directory.Exists(workingDir)) {
+                Directory.Delete(workingDir, true);
+            }
+        }
+    }  
+}
